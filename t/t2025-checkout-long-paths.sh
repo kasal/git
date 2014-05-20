48986d99a957b60fb42a44ce31668ed3c5d2bf1f
Stepan Kasal <kasal@ucw.cz>
2014-05-20 17:15:20 +0200

    squash! Win32: support long paths
    Squash in this commit text:
    Test the case is when the full pathname length of a dir is close
    to 260 (MAX_PATH).
    Bug report and an original reproducer by Andrey Rogozhnikov.
    
    Signed-off-by: Stepan Kasal <kasal@ucw.cz>
    
    The rest is for record only:
    
    The fix is to reserve space for "\*" in dirent_opendir().
    
    ROOT=C:/smiths/workspace/ATM-hourly/SLAVE/build-win7-32
    SUB1=data/data/gui/html/common/jwe/node_modules/lodash.merge/node_modules
    SUB2A=node_modules/lodash._createwrapper/node_modules
    SUB2=lodash._basecreatecallback/node_modules/lodash.bind/$SUB2A
    SUB3A=node_modules/lodash._basecreate/node_modules/lodash._isnative
    SUB3=lodash._basecreatewrapper/$SUB3A
    
    cd C:/
    mkdir -p $ROOT
    cd $ROOT
    git init
    
    mkdir -p $SUB1 $SUB2 $SUB3
    echo {} >> $SUB3/package.json
    mv ${SUB3%%/*} $SUB2
    mv ${SUB2%%/*} $SUB1
    
    cd $ROOT
    git --version
    git config core.longpaths yes
    git clean -fdx
    
    Signed-off-by: Stepan Kasal <kasal@ucw.cz>

--- t/t2025-checkout-long-paths.sh
+++ t/t2025-checkout-long-paths.sh
@@ -55,4 +55,64 @@ test_expect_success cleanup '
 	test ! -z "$debug" || rm -rf longpa~1
 '
 
+ABSPATH=$(pwd -W)
+test ${#ABSPATH} -le 209 &&
+	test_set_prereq SHORTABSPATH
+
+test_expect_success SHORTABSPATH 'clean up path close to MAX_PATH' '
+  (
+	set -e
+
+	SUB0=C:/smiths/workspace/ATM-hourly/SLAVE/build-win7-32
+	SUB1=data/data/gui/html/common/jwe/node_modules/lodash.merge/node_modules
+	SUB2=lodash._basecreatecallback/node_modules/lodash.bind/node_modules
+	SUB3=lodash._createwrapper/node_modules
+	SUB4=lodash._basecreatewrapper/node_modules
+	SUB5=lodash._basecreate/node_modules/lodash._isnative
+
+	DEEP_ABS=$SUB0/$SUB1/$SUB2/$SUB3
+	# $DEEP_ABS/$SUB4 is 258 chars long and would trigger the bug.
+
+	TESTDIR=testdir
+	PREFIX=x
+	PREFIX_ABS=$(pwd -W)/$TESTDIR/$PREFIX
+
+	# Overwrite the beginning of DEEP_ABS by PREFIX_ABS:
+	TO_CUT=$PREFIX_ABS
+	SUFFIX=$DEEP_ABS
+
+	while test -n "$TO_CUT"; do
+		TO_CUT=${TO_CUT#?}
+		SUFFIX=${SUFFIX#?}
+	done
+	# Now $PREFIX_ABS$SUFFIX/$SUB4 is also 258 chars long and is a subdirectory
+	# of current dir.
+
+	# (Note the missing slash: SUFFIX may start with a slash a we do not want
+	# to have two successive slashes.)
+	DEEP=$PREFIX$SUFFIX
+
+	mkdir "$TESTDIR"
+	cd "$TESTDIR"
+	git init
+
+	mkdir -p $DEEP
+	# Now create the critical $SUB4 that, moved under DEEP, triggers the bug.
+	# Moreover, take also variants with length +-2, to check values near MAX_PATH:
+	for SUB4A in "${SUB4}xx" "${SUB4}x" "${SUB4}" "${SUB4%?}" "${SUB4%??}"
+	do
+		# $SUB4A now represents the problem directory:
+		# create it, and populate it witha subtree and a regular file
+		# also create a regular file whose name is the same length as $SUB4A
+		mkdir -p "$SUB4A/$SUB5"
+		touch "$SUB4A/$SUB5/package.json" "$SUB4A/a_file" "${SUB4A%??}.c"
+	done
+	# move the whole tree under DEEP
+	mv "${SUB4%%/*}" "$DEEP"
+
+	git config core.longpaths yes
+	git clean -fdx
+  )
+'
+
 test_done
